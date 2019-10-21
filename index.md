1f102b0ed97173389cf8d0ff1b499d6a9accb822
Subhendu Sethi <cs13b1027@iith.ac.in>
2019-10-22 02:42:09 +0530

    Updates

--- index.md
+++
@@ -1,7 +0,0 @@
-## Welcome to subhendusethi.github.io
-
-I am [Subhendu Sethi](https://www.linkedin.com/in/subhendusethi/). Former Computer Science and Engineering graduate from [Indian Institute of Technology, Hyderabad](https://cse.iith.ac.in/index-q=node-127.html). Currently working full time as a Software Development Engineer at Amazon Development Center, Bangalore.
-
-### Site is under Construction
-![Alt Text](https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif)
-
---
+++ precache-manifest.7520e6ee48a14f7e4683e196979ea14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c5bbd1951384deec4c3d02525b0504",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baaace6db6d14f32d1b6",
+    "url": "/static/css/main.b0e425ed.chunk.css"
+  },
+  {
+    "revision": "3b7822ad02fb37ae2a17",
+    "url": "/static/js/2.3ec723af.chunk.js"
+  },
+  {
+    "revision": "baaace6db6d14f32d1b6",
+    "url": "/static/js/main.43d20a15.chunk.js"
+  },
+  {
+    "revision": "a2a5a85d36623db41e4b",
+    "url": "/static/js/runtime-main.98ec68c5.js"
+  },
+  {
+    "revision": "06c97186a9b496235d5a19f757166ae9",
+    "url": "/static/media/app-logo.06c97186.png"
+  }
+]);
